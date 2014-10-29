1fa52a30205f05d9d6036568f23ac91e30f93a2c
thongsav-usgs <thongsav@usgs.gov>
2014-10-29 17:21:33 -0500

    Refactor, no more generic paramter map.

--- nar-webservices/src/main/java/gov/usgs/cida/nar/service/DownloadServiceParameters.java
+++ nar-webservices/src/main/java/gov/usgs/cida/nar/service/DownloadServiceParameters.java
@@ -13,6 +13,18 @@ public enum DownloadServiceParameters {
 	endDateTime("End Date/Time")
 	;
 	
+	//String constants so we can use them in annotations, SHOULD MATCH ENUM NAME above
+	public static final String FORMAT_PARAM = "format";
+	public static final String DATA_TYPE_PARAM = "dataType";
+	public static final String QW_DATA_TYPE_PARAM = "qwDataType";
+	public static final String STREAM_FLOW_TYPE_PARAM = "streamFlowType";
+	public static final String CONSTITUENT_PARAM = "constituent";
+	public static final String SITE_TYPE_PARAM = "siteType";
+	public static final String STATION_ID_PARAM = "stationId";
+	public static final String STATE_PARAM = "state";
+	public static final String START_DATE_PARAM = "startDateTime";
+	public static final String END_DATE_PARAM = "endDateTime";
+	
 	private String title;
 	DownloadServiceParameters(String title) {
 		this.title = title;
--- nar-webservices/src/main/java/gov/usgs/cida/nar/service/SiteInformationService.java
+++ nar-webservices/src/main/java/gov/usgs/cida/nar/service/SiteInformationService.java
@@ -2,36 +2,44 @@ package gov.usgs.cida.nar.service;
 
 import gov.usgs.cida.nar.transform.NarOwsSiteToDelimitatedText;
 import gov.usgs.cida.nar.util.JNDISingleton;
-import gov.usgs.cida.nar.util.ServiceParameterUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Map;
+import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
-public class SiteInformationService implements INarStreamService {
+public class SiteInformationService {
 	public static final String SITE_ATTRIBUTE_TITLE = DownloadType.siteAttribute.getTitle();
 	public static final String SITE_ATTRIBUTE_OUT_FILENAME = SITE_ATTRIBUTE_TITLE.replaceAll(" ", "_");
 	
 	private static final String SITE_INFO_URL_JNDI_NAME = "nar.endpoint.ows";
 	private static final String SITE_LAYER_NAME = "NAR:JD_NFSN_sites0914";
 	
-	public void streamData(OutputStream output, final Map<String, String[]> params) throws IOException {
+	public void streamData(OutputStream output, 
+			final List<String> format,
+			final List<String> siteType,
+			final List<String> stationId,
+			final List<String> state) throws IOException {
 		Client client = ClientBuilder.newClient();
 		
-		InputStream returnStream = (InputStream)client.target(buildSiteInfoRequest(params))
+		InputStream returnStream = (InputStream)client.target(buildSiteInfoRequest(siteType,
+				stationId,
+				state))
                 .path("")
                 .request(new MediaType[] {MediaType.APPLICATION_OCTET_STREAM_TYPE})
                 .get(InputStream.class);
 		
-		new NarOwsSiteToDelimitatedText(ServiceParameterUtils.getDelimiterFromFormat(params)).transform(new InputStream[] { returnStream }, output);
+		new NarOwsSiteToDelimitatedText(",").transform(new InputStream[] { returnStream }, output);
 	}
 	
-	private static String buildSiteInfoRequest(final Map<String, String[]> params) {
+	private static String buildSiteInfoRequest( 
+			final List<String> siteType,
+			final List<String> stationId,
+			final List<String> state) {
 		//TODO build OGC filter
 		String filter = "";
 		return JNDISingleton.getInstance().getProperty(SITE_INFO_URL_JNDI_NAME) + 
