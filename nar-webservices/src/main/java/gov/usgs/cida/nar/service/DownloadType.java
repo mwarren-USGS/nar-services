7a338fae38bc715e3fff4fb7ff4a8c3cfaf759a6
thongsav-usgs <thongsav@usgs.gov>
2014-10-31 19:29:01 -0500

    Services should now generate the correct set of SOS connectors

--- nar-webservices/src/main/java/gov/usgs/cida/nar/service/DownloadType.java
+++ nar-webservices/src/main/java/gov/usgs/cida/nar/service/DownloadType.java
@@ -1,19 +1,135 @@
 package gov.usgs.cida.nar.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum DownloadType {
-	annualLoad("Annual loads"),
-	annualFlow("Annual flow"),
-	mayLoad("May loads"),
-	dailyFlow("Daily flow"),
-	discreteQw("Discrete QW"),
-	siteAttribute("Site attribute");
-	
+	annualLoad("Annual loads", 
+			Arrays.asList(
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_flow_weighted/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_concentration_mean/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_lower_95/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_mass_upper_95/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/annual_yield/REG_PRELIM_FINAL"),
+			null),
+	annualFlow("Annual flow", 
+			Arrays.asList(
+					"http://cida.usgs.gov/def/NAR/procedure/annual_flow"
+					), 
+			Arrays.asList(
+					"Q"
+					)),
+	mayLoad("May loads", 
+			Arrays.asList(
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REGREG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_concentration_flow_weighted/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_flow",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REGREG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REGREG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_lower_95/REG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/COMP",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/DAILY",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REG",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REGHIST",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REGREG_PRELIM_FINAL",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REG_3",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REG_PRELIM",
+					"http://cida.usgs.gov/def/NAR/procedure/monthly_mass_upper_95/REG_PRELIM_FINAL"),
+			null),
+	dailyFlow("Daily flow", 
+			Arrays.asList(
+					"http://cida.usgs.gov/def/NAR/procedure/daily_flow"
+					), 
+			Arrays.asList(
+					"Q"
+					)),
+	discreteQw("Discrete QW", 
+			Arrays.asList(
+					"http://cida.usgs.gov/def/NAR/procedure/discrete_concentration"
+					), 
+			null),
+	siteAttribute("Site attribute", 
+			null, 
+			null);
+
 	private String title;
-	DownloadType(String title) {
+	private List<String> procedures;
+	private List<String> observedProperties;
+	DownloadType(String title, List<String> procedures, List<String> observedProperties) {
 		this.title = title;
+		this.procedures = procedures;
+		this.observedProperties = observedProperties;
 	}
-	
+
 	public String getTitle() {
 		return title;
+	}
+	
+	public List<String> getProcedures() {
+		return procedures;
+	}
+	
+	public List<String> getObservedProperties() {
+		return observedProperties;
 	}
 }
